1e70df061d8c89ae7809971c1dece13adbd5ea86
VishwasSR07 <vishwas.s.raichur7@gmail.com>
2025-03-15 11:46:17 +0530

    Create 2. Sum of Two Numbers.py

---
+++ 2. Sum of Two Numbers.py
@@ -0,0 +1,7 @@
+# 2. Sum of Two Numbers
+def sum_two_numbers():
+    a = int(input("Enter first number Vishwas: "))
+    b = int(input("Enter second number Vishwas: "))
+    print(f"Sum Vishwas: {a + b}")
+
+sum_two_numbers()
