97f68b1c883297677280c71beef30ce4cb7cb3b1
VishwasSR07 <vishwas.s.raichur7@gmail.com>
2025-03-15 11:48:08 +0530

    Create 6. Largest of Three Numbers.py

---
+++ 6. Largest of Three Numbers.py
@@ -0,0 +1,8 @@
+# 6. Largest of Three Numbers
+def largest():
+    a = int(input("Enter first number Vishwas: "))
+    b = int(input("Enter second number Vishwas: "))
+    c = int(input("Enter third number Vishwas: "))
+    print(f"Largest Vishwas: {max(a, b, c)}")
+
+largest()
