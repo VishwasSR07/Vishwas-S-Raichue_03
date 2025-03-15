6ba28aee9189e10420ba378e384053de21a39b15
VishwasSR07 <vishwas.s.raichur7@gmail.com>
2025-03-15 11:58:59 +0530

    Create 20. Using the math Module .py

---
+++ 20. Using the math Module .py
@@ -0,0 +1,7 @@
+# 20. Using the math Module  
+import math
+
+num = 5
+print(f"Vishwas, square root of {num} is: {math.sqrt(num)}")
+print(f"Factorial of {num} is: {math.factorial(num)}")
+print(f"Sine of {num} is: {math.sin(num)}")
