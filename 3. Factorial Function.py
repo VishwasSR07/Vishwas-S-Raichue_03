cba0c836704ce7ceeb29911d41e22e3d3bab2a3f
VishwasSR07 <vishwas.s.raichur7@gmail.com>
2025-03-15 11:46:41 +0530

    Create 3. Factorial Function.py

---
+++ 3. Factorial Function.py
@@ -0,0 +1,8 @@
+# 3. Factorial Function
+def factorial(n):
+    if n == 0:
+        return 1
+    return n * factorial(n - 1)
+
+n = int(input("Enter a number Vishwas: "))
+print(f"Factorial Vishwas: {factorial(n)}")
