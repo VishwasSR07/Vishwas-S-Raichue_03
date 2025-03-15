c48d0153476be09b1f93cc0d9f86e2f66be6f42c
VishwasSR07 <vishwas.s.raichur7@gmail.com>
2025-03-15 11:51:13 +0530

    Create 11. Even or Odd Function.py

---
+++ 11. Even or Odd Function.py
@@ -0,0 +1,9 @@
+# 11. Even or Odd Function
+def even_or_odd():
+    num = int(input("Enter a number Vishwas: "))
+    if num % 2 == 0:
+        print(f"{num} is even Vishwas")
+    else:
+        print(f"{num} is odd Vishwas")
+
+even_or_odd()
